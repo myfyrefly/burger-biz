2f608ecf1c73da59ef34ab844c74dd9166692d23
Lemonade Kidd <smejia03@gmail.com>
2020-04-17 14:34:20 -0500

    file structure setup

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers
+(burger_name, devoured)
+VALUES 
+('cheeseburger', true),
+('bacon burger', false),
+('spam burger', false), 
+('veggie burger', false),
+('bbq burger', true);
