dd701e9f08855fa4b09fa8981e545ef67f1617cb
Lemonade Kidd <smejia03@gmail.com>
2020-04-22 23:40:21 -0500

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql');
 // const util = require('util');
+var connection;
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "R3ydeTigres!",
+    database: "burgers_db"
+  });
+}
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "R3ydeTigres!",
-  database: "burgers_db"
-});
 
 connection.connect(function(err) {
     if(err) {
